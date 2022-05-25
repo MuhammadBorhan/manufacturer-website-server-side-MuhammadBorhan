945a938a2c429d5ab09c36717ffa2fb04ef09325
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-26 02:49:02 +0600

    delete api create for delete from products

--- index.js
+++ index.js
@@ -210,6 +210,14 @@ async function run() {
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         });
+
+        // Delete single product of products
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
